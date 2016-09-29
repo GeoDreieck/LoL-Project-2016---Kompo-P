495e150fb92db4cf739c113a93416965ef172b6d
Julian Feil <j.feil@ostfalia.de>
2016-09-29 23:58:41 +0200

    Readded something that was autodeleted by VS.

--- LoL Dex 2016 Kompo-P/CompUI/Runes.Designer.cs
+++ LoL Dex 2016 Kompo-P/CompUI/Runes.Designer.cs
@@ -31,8 +31,8 @@
             this.RunesIconBox = new System.Windows.Forms.PictureBox();
             this.rune_level_box = new System.Windows.Forms.ComboBox();
             this.rune_proberty_box = new System.Windows.Forms.ComboBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
+            this.RuneLevelLabel = new System.Windows.Forms.Label();
+            this.RuneKindLabel = new System.Windows.Forms.Label();
             this.lView_Runes = new System.Windows.Forms.ListView();
             this.ID = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.Runenname = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
@@ -82,27 +82,27 @@
             this.rune_proberty_box.TabIndex = 16;
             this.rune_proberty_box.SelectedIndexChanged += new System.EventHandler(this.FilterChanged);
             // 
-            // label1
+            // RuneLevelLabel
             // 
-            this.label1.AutoSize = true;
-            this.label1.BackColor = System.Drawing.Color.Transparent;
-            this.label1.Font = new System.Drawing.Font("Verdana", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(369, 41);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(98, 14);
-            this.label1.TabIndex = 17;
-            this.label1.Text = "Runen - Level";
+            this.RuneLevelLabel.AutoSize = true;
+            this.RuneLevelLabel.BackColor = System.Drawing.Color.Transparent;
+            this.RuneLevelLabel.Font = new System.Drawing.Font("Verdana", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.RuneLevelLabel.Location = new System.Drawing.Point(369, 41);
+            this.RuneLevelLabel.Name = "RuneLevelLabel";
+            this.RuneLevelLabel.Size = new System.Drawing.Size(98, 14);
+            this.RuneLevelLabel.TabIndex = 17;
+            this.RuneLevelLabel.Text = "Runen - Level";
             // 
-            // label2
+            // RuneKindLabel
             // 
-            this.label2.AutoSize = true;
-            this.label2.BackColor = System.Drawing.Color.Transparent;
-            this.label2.Font = new System.Drawing.Font("Verdana", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(532, 41);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(82, 14);
-            this.label2.TabIndex = 18;
-            this.label2.Text = "Runen - Art";
+            this.RuneKindLabel.AutoSize = true;
+            this.RuneKindLabel.BackColor = System.Drawing.Color.Transparent;
+            this.RuneKindLabel.Font = new System.Drawing.Font("Verdana", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.RuneKindLabel.Location = new System.Drawing.Point(532, 41);
+            this.RuneKindLabel.Name = "RuneKindLabel";
+            this.RuneKindLabel.Size = new System.Drawing.Size(82, 14);
+            this.RuneKindLabel.TabIndex = 18;
+            this.RuneKindLabel.Text = "Runen - Art";
             // 
             // lView_Runes
             // 
@@ -143,8 +143,8 @@
             this.ClientSize = new System.Drawing.Size(748, 525);
             this.Controls.Add(this.MainContentPanel);
             this.Controls.Add(this.lView_Runes);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.RuneKindLabel);
+            this.Controls.Add(this.RuneLevelLabel);
             this.Controls.Add(this.rune_proberty_box);
             this.Controls.Add(this.rune_level_box);
             this.Controls.Add(this.RunesIconBox);
@@ -163,8 +163,8 @@
         private System.Windows.Forms.PictureBox RunesIconBox;
         private System.Windows.Forms.ComboBox rune_level_box;
         private System.Windows.Forms.ComboBox rune_proberty_box;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label RuneLevelLabel;
+        private System.Windows.Forms.Label RuneKindLabel;
         private System.Windows.Forms.ListView lView_Runes;
         private System.Windows.Forms.ColumnHeader ID;
         private System.Windows.Forms.ColumnHeader Runenname;
